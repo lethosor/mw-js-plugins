1daa93681e3d70e19ab64ed9d8ce482560bcac97
Lethosor <lethosor@gmail.com>
2013-09-23 19:53:38 -0400

    Update plug.js
    Basic initialization framework

--- plug.js
+++ plug.js
@@ -1,5 +1,9 @@
 jQuery(function($){
-	plug = {};
+	plug = {list:[]};
+	plug.register = function(name, init){
+		// Initialize plugin
+		plug.list[name] = init(plug);
+	};
 	
 	window.plug = plug;
 	$(document).trigger('pluginjs-loaded');
